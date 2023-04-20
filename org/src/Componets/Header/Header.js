5eb617e51132ec34c290d2fc302869e204b42674
Orion <wilman3d@hotmail.com>
2023-04-20 12:23:33 -0500

    guardado porsiaca descasito

---
+++ org/src/Componets/Header/Header.js
@@ -0,0 +1,11 @@
+import './Header.css';
+
+function Header() {
+	return (
+		<header className='header'>
+			<img className='img' src='/img/header.png' alt='header' />
+		</header>
+	);
+}
+
+export default Header;
